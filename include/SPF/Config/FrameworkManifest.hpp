5326edc31f24c142ea0ee2163db947328a81e106
TrackAndTruckDevs <spfframework@gmail.com>
2026-08-23 20:03:09 +0300

    Release 1.2.2

--- include/SPF/Config/FrameworkManifest.hpp
+++ include/SPF/Config/FrameworkManifest.hpp
@@ -28,7 +28,7 @@ inline const ManifestData& GetFrameworkManifestData() {
       .info =
         {
           .name = "SPF Framework",
-          .version = "1.2.1",
+          .version = "1.2.2",
           .author = "Track'n'Truck Devs",
           .descriptionKey = "description.detailed",  // key in the translation file
           .descriptionLiteral = "",                  // if there is no translation, you can write a description here
